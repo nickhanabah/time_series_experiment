5c12b0b9cef27f2489c6a2481a377f6a18cc4a22
nickhanabah <102148580+nickhanabah@users.noreply.github.com>
2024-06-05 10:09:45 +0000

    M

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -39,6 +39,12 @@ def plot_multistep_forecast(test_data, neural_net, future_steps, number_of_forec
     else: 
         target = target_list
         alpha = 1
+
+    print('target')
+    print(target)
+
+    print('output')
+    print(output_list)
     
     fig = plt.figure(figsize=(15, 15))
     if future_steps > 1: 
@@ -55,27 +61,3 @@ def plot_multistep_forecast(test_data, neural_net, future_steps, number_of_forec
     plt.legend(loc="upper left")
     plt.xlabel("Time Steps")
     plt.ylabel("Oil Temparature (Target variable)")
-
-def evaluate_on_test_data(test_data, neural_net): 
-    running_test_mae  = 0.
-    running_test_mse  = 0.
-    running_test_mape = 0.
-    test_counter = 0
-
-    for i, data in enumerate(test_data):
-        inputs, test_labels = data
-        test_labels = test_labels.squeeze(0).float()
-        output = neural_net(inputs)
-        output_array = output.detach().cpu().numpy()
-        test_labels_array = test_labels.squeeze(2).detach().cpu().numpy()
-        mae, mse, mape= metric(pred=output_array, true=test_labels_array)
-        running_test_mae  += mae
-        running_test_mse  += mse
-        running_test_mape += mape
-        test_counter += neural_net.batch_size
-
-    print("Test metrics: -------")
-    print(f"Validation MAE is {running_test_mae/test_counter}.")
-    print(f"Validation MSE is {running_test_mse/test_counter}.")
-    print(f"Validation MAPE is {running_test_mape/test_counter}.")
-    print("---------------------------")
