f06ff4fbc767dd9ec77137d7e4ee8ec667212978
nickhanabah <102148580+nickhanabah@users.noreply.github.com>
2024-08-18 10:43:16 +0000

    Added Moe functionality

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -490,3 +490,57 @@ class ARNet(nn.Module):
             return normal_object
         else:
             return y_hat
+
+
+class Gating(nn.Module):
+    def __init__(self, input_dim,
+                 num_experts, dropout_rate=0.1):
+        super(Gating, self).__init__()
+
+        # Layers
+        self.layer1 = nn.Linear(input_dim, 128)
+        self.dropout1 = nn.Dropout(dropout_rate)
+
+        self.layer2 = nn.Linear(128, 256)
+        self.leaky_relu1 = nn.LeakyReLU()
+        self.dropout2 = nn.Dropout(dropout_rate)
+
+        self.layer3 = nn.Linear(256, 128)
+        self.leaky_relu2 = nn.LeakyReLU()
+        self.dropout3 = nn.Dropout(dropout_rate)
+
+        self.layer4 = nn.Linear(128, num_experts)
+
+    def forward(self, x):
+        x = torch.relu(self.layer1(x))
+        x = self.dropout1(x)
+
+        x = self.layer2(x)
+        x = self.leaky_relu1(x)
+        x = self.dropout2(x)
+
+        x = self.layer3(x)
+        x = self.leaky_relu2(x)
+        x = self.dropout3(x)
+
+        return torch.softmax(self.layer4(x), dim=1)
+
+
+class MoE(nn.Module):
+    def __init__(self, trained_experts:list[ARNet], input_dim:int):
+        super(MoE, self).__init__()
+        self.experts = nn.ModuleList(trained_experts)
+
+        for expert in self.experts:
+            for param in expert.parameters():
+                param.requires_grad = False
+
+        num_experts = len(trained_experts)
+        self.gating = Gating(input_dim, num_experts)
+
+    def forward(self, x):
+        weights = self.gating(x)
+        outputs = torch.stack(
+            [expert(x) for expert in self.experts], dim=2)
+        weights = weights.unsqueeze(1).expand_as(outputs)
+        return torch.sum(outputs * weights, dim=2)
--- utils/training_utils.py
+++ utils/training_utils.py
@@ -1,5 +1,5 @@
 import torch
-from utils.model_utils import ARNet, set_seed, normal_loss
+from utils.model_utils import ARNet, set_seed, normal_loss, MoE
 from torch.utils.data import DataLoader
 from utils.data_utils import TimeSeriesDataset
 from utils.metrics import metric
@@ -20,24 +20,30 @@ def train(
     decomp_kernel_size=7,
     batch_size=8,
     model="rlinear",
+    moe: bool = False, 
+    num_of_experts: int = 1, 
     modelling_task="univariate",
     density=False,
     depth = 'shallow'
 ):
 
-    set_seed()
-    net = ARNet(
-        p_lag=p_lag,
-        n_continous_features=n_continous_features,
-        n_categorial_features=n_categorial_features,
-        future_steps=future_steps,
-        decomp_kernel_size=decomp_kernel_size,
-        batch_size=batch_size,
-        model=model,
-        modelling_task=modelling_task,
-        density=density,
-        depth = depth
-    )
+    #set_seed()
+    untrained_experts = []
+    for _ in range(num_of_experts): 
+        net = ARNet(
+                p_lag=p_lag,
+                n_continous_features=n_continous_features,
+                n_categorial_features=n_categorial_features,
+                future_steps=future_steps,
+                decomp_kernel_size=decomp_kernel_size,
+                batch_size=batch_size,
+                model=model,
+                modelling_task=modelling_task,
+                density=density,
+                depth = depth
+            )
+        untrained_experts.append(net)
+
     train_data = DataLoader(
         TimeSeriesDataset(
             training_df,
@@ -68,123 +74,224 @@ def train(
     torch.set_grad_enabled(True)
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
-    for epoch in range(epochs):
-        train_counter = 0
-        val_counter = 0
-        running_train_loss = 0.0
-        running_val_loss = 0.0
-        running_train_mae = 0.0
-        running_train_mse = 0.0
-        running_val_mae = 0.0
-        running_val_mse = 0.0
+    trained_experts = []
+    for i in range(num_of_experts): 
+        print(f'Started training expert {i +1}/{num_of_experts}')
+        net = untrained_experts[i]
+        for epoch in range(epochs):
+            train_counter = 0
+            val_counter = 0
+            running_train_loss = 0.0
+            running_val_loss = 0.0
+            running_train_mae = 0.0
+            running_train_mse = 0.0
+            running_val_mae = 0.0
+            running_val_mse = 0.0
 
-        if epoch + 1 != 1 and (epoch + 1) % 2 == 0:
-            learning_rate = learning_rate / 2
-            optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
-        print(f"Current learning rate is : {learning_rate}")
-        print("---------------------------")
-        for i, data in enumerate(train_data):
-            inputs, labels = data
-            labels = labels.squeeze(0).float()
+            if epoch + 1 != 1 and (epoch + 1) % 2 == 0:
+                learning_rate = learning_rate / 2
+                optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+            print(f"Current learning rate is : {learning_rate}")
+            print("---------------------------")
+            for i, data in enumerate(train_data):
+                inputs, labels = data
+                labels = labels.squeeze(0).float()
 
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            if density:
-                loss = normal_loss(outputs, labels.squeeze(1))
-            else:
-                if modelling_task == "multivariate":
-                    loss = net.criterion(
-                        outputs,
-                        labels[:, 0:(n_continous_features), :].reshape(outputs.shape),
-                    )
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                if density:
+                    loss = normal_loss(outputs, labels.squeeze(1))
                 else:
-                    loss = net.criterion(outputs, labels.squeeze(1))
+                    if modelling_task == "multivariate":
+                        loss = net.criterion(
+                            outputs,
+                            labels[:, 0:(n_continous_features), :].reshape(outputs.shape),
+                        )
+                    else:
+                        loss = net.criterion(outputs, labels.squeeze(1))
 
-            loss.backward()
-            # torch.nn.utils.clip_grad_norm_(net.parameters(), 1)
-            optimizer.step()
+                loss.backward()
+                # torch.nn.utils.clip_grad_norm_(net.parameters(), 1)
+                optimizer.step()
+                if density == False:
+                    outputs_array = outputs.detach().cpu().numpy()
+                    labels_array = labels.squeeze(2).detach().cpu().numpy()
+                    mae, mse = metric(pred=outputs_array, true=labels_array)
+                    running_train_mae += mae
+                    running_train_mse += mse
+
+                running_train_loss += loss.item()
+                train_counter += batch_size
+                if train_counter % 5000 == 0:
+                    print(
+                        f"Current (running) training loss at iteration {train_counter} : {running_train_loss/train_counter}"
+                    )
+            train_loss_list.append(running_train_loss / train_counter)
+
             if density == False:
-                outputs_array = outputs.detach().cpu().numpy()
-                labels_array = labels.squeeze(2).detach().cpu().numpy()
-                mae, mse = metric(pred=outputs_array, true=labels_array)
-                running_train_mae += mae
-                running_train_mse += mse
+                for i, data in enumerate(val_data):
+                    inputs, test_labels = data
+                    test_labels = test_labels.squeeze(0).float()
+                    output = net(inputs)
+                    if modelling_task == "multivariate":
+                        val_loss = net.criterion(
+                            outputs,
+                            test_labels[:, 0:(n_continous_features), :].reshape(
+                                outputs.shape
+                            ),
+                        )
+                    else:
+                        val_loss = net.criterion(output, test_labels.squeeze(1))
 
-            running_train_loss += loss.item()
-            train_counter += batch_size
-            if train_counter % 5000 == 0:
-                print(
-                    f"Current (running) training loss at iteration {train_counter} : {running_train_loss/train_counter}"
-                )
-        train_loss_list.append(running_train_loss / train_counter)
+                    running_val_loss += val_loss.item()
 
-        if density == False:
-            for i, data in enumerate(val_data):
-                inputs, test_labels = data
-                test_labels = test_labels.squeeze(0).float()
-                output = net(inputs)
-                if modelling_task == "multivariate":
-                    val_loss = net.criterion(
-                        outputs,
-                        test_labels[:, 0:(n_continous_features), :].reshape(
-                            outputs.shape
-                        ),
+                    output_array = output.detach().cpu().numpy()
+                    test_labels_array = test_labels.squeeze(2).detach().cpu().numpy()
+                    mae, mse = metric(pred=output_array, true=test_labels_array)
+                    running_val_mae += mae
+                    running_val_mse += mse
+                    val_counter += batch_size
+                val_loss_list.append(running_val_loss / val_counter)
+
+                if epoch % 1 == 0:
+                    print("")
+                    print(f"Epoch {epoch}: ")
+                    print("")
+                    print("Train metrics: -------")
+                    print(f"Running (training) loss is {running_train_loss/train_counter}.")
+                    print(f"Training MAE is {running_train_mae/train_counter}.")
+                    print(f"Training MSE is {running_train_mse/train_counter}.")
+                    print("")
+                    print("Test metrics: -------")
+                    print(f"Running (test) loss is {running_val_loss/val_counter}.")
+                    print(f"Test MAE is {running_val_mae/val_counter}.")
+                    print(f"Test MSE is {running_val_mse/val_counter}.")
+                    print("---------------------------")
+
+                    final_test_result = f"""
+    Test metrics: -------
+    Running (test) loss is {running_val_loss/val_counter}
+    Test MAE is {running_val_mae/val_counter}
+    Test MSE is {running_val_mse/val_counter}
+    ---------------------------
+    Parameters: -------
+    epochs = {epochs}
+    p_lag = {p_lag}
+    future_steps = {future_steps}
+    n_continous_features = {n_continous_features}
+    n_categorial_features = {n_categorial_features}
+    feature_columns = {feature_columns}
+    target_column = {target_column} 
+    learning_rate = {learning_rate} 
+    decomp_kernel_size = {decomp_kernel_size}
+    batch_size = {batch_size} 
+    model = {model}
+    modelling_task = {modelling_task}
+    density = {density}
+    depth = {depth}
+    ---------------------------
+                    """
+                    f = open(
+                        f"/workspaces/time_series_experiment/results/{dataset_name}df_{net.model}_{future_steps}fs_{net.model}_{p_lag}plag.txt",
+                        "w",
                     )
+                    f.write(final_test_result)
+                    f.close()
+        trained_experts.append(net)
+    if moe: 
+        moe_model = MoE(trained_experts)
+
+        optimizer_moe = torch.optim.Adam(moe_model.parameters(), lr=learning_rate)
+        for epoch in range(epochs):
+            train_counter = 0
+            val_counter = 0
+            running_train_loss = 0.0
+            running_val_loss = 0.0
+            running_train_mae = 0.0
+            running_train_mse = 0.0
+            running_val_mae = 0.0
+            running_val_mse = 0.0
+
+            if epoch + 1 != 1 and (epoch + 1) % 2 == 0:
+                learning_rate = learning_rate / 2
+                optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+            print(f"Current learning rate is : {learning_rate}")
+            print("---------------------------")
+            for i, data in enumerate(train_data):
+                inputs, labels = data
+                labels = labels.squeeze(0).float()
+
+                optimizer_moe.zero_grad()
+                outputs = moe_model(inputs)
+                if density:
+                    loss = normal_loss(outputs, labels.squeeze(1))
                 else:
-                    val_loss = net.criterion(output, test_labels.squeeze(1))
+                    if modelling_task == "multivariate":
+                        loss = net.criterion(
+                            outputs,
+                            labels[:, 0:(n_continous_features), :].reshape(outputs.shape),
+                        )
+                    else:
+                        loss = net.criterion(outputs, labels.squeeze(1))
 
-                running_val_loss += val_loss.item()
+                loss.backward()
+                # torch.nn.utils.clip_grad_norm_(net.parameters(), 1)
+                optimizer.step()
+                if density == False:
+                    outputs_array = outputs.detach().cpu().numpy()
+                    labels_array = labels.squeeze(2).detach().cpu().numpy()
+                    mae, mse = metric(pred=outputs_array, true=labels_array)
+                    running_train_mae += mae
+                    running_train_mse += mse
 
-                output_array = output.detach().cpu().numpy()
-                test_labels_array = test_labels.squeeze(2).detach().cpu().numpy()
-                mae, mse = metric(pred=output_array, true=test_labels_array)
-                running_val_mae += mae
-                running_val_mse += mse
-                val_counter += batch_size
-            val_loss_list.append(running_val_loss / val_counter)
+                running_train_loss += loss.item()
+                train_counter += batch_size
+                if train_counter % 5000 == 0:
+                    print(
+                        f"Current (running) training loss at iteration {train_counter} : {running_train_loss/train_counter}"
+                    )
+            train_loss_list.append(running_train_loss / train_counter)
 
-            if epoch % 1 == 0:
-                print("")
-                print(f"Epoch {epoch}: ")
-                print("")
-                print("Train metrics: -------")
-                print(f"Running (training) loss is {running_train_loss/train_counter}.")
-                print(f"Training MAE is {running_train_mae/train_counter}.")
-                print(f"Training MSE is {running_train_mse/train_counter}.")
-                print("")
-                print("Test metrics: -------")
-                print(f"Running (test) loss is {running_val_loss/val_counter}.")
-                print(f"Test MAE is {running_val_mae/val_counter}.")
-                print(f"Test MSE is {running_val_mse/val_counter}.")
-                print("---------------------------")
+            if density == False:
+                for i, data in enumerate(val_data):
+                    inputs, test_labels = data
+                    test_labels = test_labels.squeeze(0).float()
+                    output = moe_model(inputs)
+                    if modelling_task == "multivariate":
+                        val_loss = net.criterion(
+                            outputs,
+                            test_labels[:, 0:(n_continous_features), :].reshape(
+                                outputs.shape
+                            ),
+                        )
+                    else:
+                        val_loss = net.criterion(output, test_labels.squeeze(1))
 
-                final_test_result = f"""
-Test metrics: -------
-Running (test) loss is {running_val_loss/val_counter}
-Test MAE is {running_val_mae/val_counter}
-Test MSE is {running_val_mse/val_counter}
----------------------------
-Parameters: -------
-epochs = {epochs}
-p_lag = {p_lag}
-future_steps = {future_steps}
-n_continous_features = {n_continous_features}
-n_categorial_features = {n_categorial_features}
-feature_columns = {feature_columns}
-target_column = {target_column} 
-learning_rate = {learning_rate} 
-decomp_kernel_size = {decomp_kernel_size}
-batch_size = {batch_size} 
-model = {model}
-modelling_task = {modelling_task}
-density = {density}
-depth = {depth}
----------------------------
-                """
-                f = open(
-                    f"/workspaces/time_series_experiment/results/{dataset_name}df_{net.model}_{future_steps}fs_{net.model}_{p_lag}plag.txt",
-                    "w",
-                )
-                f.write(final_test_result)
-                f.close()
-    return net
+                    running_val_loss += val_loss.item()
+
+                    output_array = output.detach().cpu().numpy()
+                    test_labels_array = test_labels.squeeze(2).detach().cpu().numpy()
+                    mae, mse = metric(pred=output_array, true=test_labels_array)
+                    running_val_mae += mae
+                    running_val_mse += mse
+                    val_counter += batch_size
+                val_loss_list.append(running_val_loss / val_counter)
+
+                if epoch % 1 == 0:
+                    print("")
+                    print(f"Epoch {epoch}: ")
+                    print("")
+                    print("Train metrics: -------")
+                    print(f"Running (training) loss is {running_train_loss/train_counter}.")
+                    print(f"Training MAE is {running_train_mae/train_counter}.")
+                    print(f"Training MSE is {running_train_mse/train_counter}.")
+                    print("")
+                    print("Test metrics: -------")
+                    print(f"Running (test) loss is {running_val_loss/val_counter}.")
+                    print(f"Test MAE is {running_val_mae/val_counter}.")
+                    print(f"Test MSE is {running_val_mse/val_counter}.")
+                    print("---------------------------")
+        return moe_model
+    
+    else: 
+        return net
